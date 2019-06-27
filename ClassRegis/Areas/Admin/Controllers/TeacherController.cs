f54660e1581d9d2160b13eadeca5e4c09b4ba4bd
satiando5826 <sathianpornkhamdee@gmail.com>
2019-06-27 16:42:28 +0700

    Teacher manage
    +REBASE

---
+++ ClassRegis/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassRegis.Data;
+using ClassRegis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRegis.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TeacherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TeacherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Teachers);
+        }
+
+        //get Create Teacher
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //post Create Teacher
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Teachers teachers)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(teachers);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teachers);
+        }
+
+        //get Detail
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await _db.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+            
+            return View(teacher);
+        }
+
+        //get Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tearcher = await _db.Teachers.FindAsync(id);
+            if (tearcher == null)
+            {
+                return NotFound();
+            }
+
+            return View(tearcher);
+        }
+
+        //post Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var teacher = await _db.Teachers.FindAsync(id);
+            _db.Teachers.Remove(teacher);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //get Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await _db.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return View(teacher);
+        }
+
+        //post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Teachers teachers)
+        {
+            if (id != teachers.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(teachers);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teachers);
+        }
+
+    }
+}
